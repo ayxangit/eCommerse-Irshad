cb99e6aa96777eccfca55cb13e74712864f7ac04
ayxangit <ayxanelxanli98@gmail.com>
2022-08-06 16:50:28 +0400

    to be contiuned

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+// start slide 
+      let swiper = new Swiper(".mySwiper", {
+        spaceBetween: 30,
+        // autoplay:true,
+        // loop:true,
+        speed:400,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+      });
