30fb0cc6c3428d1d54c043883d60e92b0659e83b
ayxangit <ayxanelxanli98@gmail.com>
2022-08-07 19:41:52 +0400

    end of section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
 
-// start slide 
+// start sale slide 
       let swiper = new Swiper(".mySwiper", {
         spaceBetween: 30,
-        // autoplay:true,
-        // loop:true,
-        speed:400,
+        autoplay:true,
+        loop:true,
+        speed:500,
         pagination: {
           el: ".swiper-pagination",
           clickable: true,
         },
       });
+    // end
+
+// start time slide
+let swiperTime = new Swiper(".mySwiper2", {
+    autoplay:true,
+    loop:true,
+    speed:300
+});
+
