c4c1a1163a62399f6624ac16cfb7b01b4d508fe9
ayxangit <ayxanelxanli98@gmail.com>
2022-08-13 18:45:42 +0400

    end of section

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,8 +70,54 @@ document.querySelector(".videoSlayd").addEventListener("click",()=>{
   
 })
 
-document.querySelector(".pay_icon").addEventListener("click",()=>{
+document.querySelector(".video_icon").addEventListener("click",()=>{
  document.querySelector(".videoCard").style.visibility="hidden";
+})
+
+document.querySelector(".videoSlayd2").addEventListener("click",()=>{
+  document.querySelector(".videoCard2").style.visibility="visible";
+  
+})
+
+document.querySelector(".video_icon2").addEventListener("click",()=>{
+ document.querySelector(".videoCard2").style.visibility="hidden";
+})
+
+document.querySelector(".videoSlayd3").addEventListener("click",()=>{
+  document.querySelector(".videoCard3").style.visibility="visible";
+  
+})
+
+document.querySelector(".video_icon3").addEventListener("click",()=>{
+ document.querySelector(".videoCard3").style.visibility="hidden";
 })
 // end
 
+// start countdown reason section
+
+
+
+const days = document.querySelector("#days")
+const hours = document.querySelector("#hours")
+const minutes = document.querySelector("#minutes")
+const seconds = document.querySelector("#seconds")
+
+
+const currentDate = new Date().getFullYear();
+
+const saleDate = new Date(` January 01 ${currentDate+1} 00:00:00`);
+
+function saleTimeReason() {
+    const currentTime = new Date();
+    const diffrent = saleDate-currentTime;
+
+    
+    const d = Math.floor(diffrent/1000/60/60/24)%30;
+    const h = Math.floor(diffrent/1000/60/60)%24;
+    const m = Math.floor(diffrent/1000/60)%60;
+    const s = Math.floor(diffrent/1000)%60;
+
+   days.innerHTML=d;
+}
+
+setInterval(saleTimeReason,1000)
