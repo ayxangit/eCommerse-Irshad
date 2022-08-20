2070214852a1bf65cf128048a7058194c0bf0c79
ayxangit <ayxanelxanli98@gmail.com>
2022-08-20 16:01:48 +0400

    end of compain page

---
+++ assets/js/compain2.js
@@ -0,0 +1,10 @@
+// paying function
+document.querySelector(".mothly").addEventListener("click",()=>{
+    document.querySelector(".content").style.visibility="visible";
+    
+ })
+ 
+ document.querySelector(".pay_icon").addEventListener("click",()=>{
+   document.querySelector(".content").style.visibility="hidden";
+ })
+ // end
